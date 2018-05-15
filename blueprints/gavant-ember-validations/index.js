d4097b722dab225ff5bbad0ee62a1a225ac1626d
Adam Baker <adambaker@adams-mbp.gavant.com>
2018-05-15 10:20:36 -0400

    Adding ember cli sass to project

---
+++ blueprints/gavant-ember-validations/index.js
@@ -0,0 +1,31 @@
+/* eslint-env node */
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {
+    normalizeEntityName() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+    },
+
+    afterInstall() {
+        let importStatement = '\n@import "gavant-ember-validations";\n';
+
+        let stylePath = path.join('app', 'styles');
+        let file = path.join(stylePath, 'app.scss');
+
+        if (!fs.existsSync(stylePath)) {
+            fs.mkdirSync(stylePath);
+        }
+        if (fs.existsSync(file)) {
+            this.ui.writeLine(`Added import statement to ${file}`);
+            return this.insertIntoFile(file, importStatement, {});
+        } else {
+            fs.writeFileSync(file, importStatement);
+            this.ui.writeLine(`Created ${file}`);
+        }
+    }
+};

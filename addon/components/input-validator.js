30720248d1987cbf48bcfd8c22a105558f242ef2
Bill Dami <bdami@gavant.com>
2019-03-25 12:58:04 -0400

    remove jquery usage to prep for ember octane. add ember-cli-release/changelog/github-pages for release flow

--- addon/components/input-validator.js
+++ addon/components/input-validator.js
@@ -47,10 +47,14 @@ export default Component.extend({
         return set(this, 'hasFocusedOut', true);
     },
 
-    didRender() {
+    didInsertElement() {
         this._super(...arguments);
-        const id = this.$('input, select, textarea').first().attr('id');
-        this.$('label.input-validator-label').attr('for', id);
+        const input = this.element.querySelector('input, select, textarea');
+        const label = this.element.querySelector('label.input-validator-label');
+        if(input && label) {
+            label.setAttribute('for', input.id);
+        }
+
         defineProperty(this, 'error', reads(`targetView.changeset.error.${get(this, 'target')}.validation`));
     }
 });

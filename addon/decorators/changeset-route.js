da1abd72c7b3cb56a77292c8d85eab10dc3f05ba
Adam Baker <bakerac4@gmail.com>
2019-03-27 19:32:41 -0400

    Adding super

--- addon/decorators/changeset-route.js
+++ addon/decorators/changeset-route.js
@@ -5,7 +5,8 @@ import createChangeset from './create-changeset';
 export default function changesetRoute(RouteSubclass) {
     class ChangesetRoute extends RouteSubclass {
         setupController(controller, model) {
-            super.setupController(controller, model);
+            this._super(...arguments);
+            // super.setupController(controller, model);
             const validations = this.validations;
             const cloneAttrs = this.cloneAttrs;
             assert('You must provide a validations object on the "validations" property!', !isNone(validations));

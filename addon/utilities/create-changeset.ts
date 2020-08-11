fcbccc3963e8ab31ec290ab2686db7d781719c94
Adam Baker <abaker@gavant.com>
2020-08-11 12:58:29 -0400

    Improve typings for generic changeset

--- addon/utilities/create-changeset.ts
+++ addon/utilities/create-changeset.ts
@@ -3,20 +3,20 @@ import lookupValidator from 'ember-changeset-validations';
 import { BufferedChangeset, ValidatorMap } from 'ember-changeset/types';
 import DS from 'ember-data';
 
-export type ModelChangeset<T> = BufferedChangeset & T;
+export type GenericChangeset<T> = BufferedChangeset & (T extends DS.Model ? Pick<T, Exclude<keyof T, keyof DS.Model>> : T) & { data: T };
 
 /**
  * Creates a changeset
  * @param {DS.Model | object} target The target object you want to create a changeset of. Generally a ED Model or POJO
  * @param {ValidatorMap} validation The validation for the object your creating a changeset for
- * @returns {ModelChangeset<T>}
+ * @returns {GenericChangeset<T>}
  */
-export default function createChangeset<T>(target: DS.Model | object, validations: ValidatorMap): ModelChangeset<T> {
+export default function createChangeset<T>(target: DS.Model | object, validations: ValidatorMap): GenericChangeset<T> {
     const changeset = (
         validations
         ? Changeset(target, lookupValidator(validations), validations)
         : Changeset(target)
-    ) as ModelChangeset<T>;
+    ) as GenericChangeset<T>;
 
     return changeset;
 }

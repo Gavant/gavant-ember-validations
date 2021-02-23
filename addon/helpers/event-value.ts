76636ee477d4980bce13382ba464b86db3c9fda1
Adam Baker <bakerac4@gmail.com>
2021-02-23 11:24:41 -0500

    Fixes for PR

--- addon/helpers/event-value.ts
+++ addon/helpers/event-value.ts
@@ -1,5 +1,12 @@
 import { helper } from '@ember/component/helper';
 
+/**
+ * A wrapper/helper to get the value from an input event
+ *
+ * @export
+ * @param {[(value: string) => void]} [fn]
+ * @return {*}
+ */
 export function eventValue([fn]: [(value: string) => void]) {
     return (event: InputEvent) => fn((event.target as HTMLInputElement)?.value);
 }

b33fc008c4b7fa925b7dc4b0575cdd77e9391a72
Adam Baker <bakerac4@gmail.com>
2021-02-23 10:22:05 -0500

    Adding in nested object handlers

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,16 +1,27 @@
 import Route from '@ember/routing/route';
 import ChangesetRoute from '@gavant/ember-validations/mixins/changeset-route';
-import { validatePresence, validateNumber } from 'ember-changeset-validations/validators';
+import {
+    validatePresence,
+    validateNumber,
+} from 'ember-changeset-validations/validators';
 import createChangeset from '@gavant/ember-validations/utilities/create-changeset';
 
 const Validations = {
-    name: [validatePresence({presence: true, ignoreBlank: true})],
-    num: [validatePresence({presence: true, ignoreBlank: true}), validateNumber({gte: 1, lte: 100, integer: true})],
-    radio: [validatePresence({presence: true})]
+    name: [validatePresence({ presence: true, ignoreBlank: true })],
+    num: [
+        validatePresence({ presence: true, ignoreBlank: true }),
+        validateNumber({ gte: 1, lte: 100, integer: true }),
+    ],
+    radio: [validatePresence({ presence: true })],
+    nestedItem: {
+        much: {
+            wow: [validatePresence({ presence: true, ignoreBlank: true })],
+        },
+    },
 };
 
 const childValidations = {
-    foo: [validatePresence({presence: true, ignoreBlank: true})]
+    foo: [validatePresence({ presence: true, ignoreBlank: true })],
 };
 
 export default class Application extends ChangesetRoute(Route) {
@@ -20,7 +31,7 @@ export default class Application extends ChangesetRoute(Route) {
         return {
             name: null,
             num: 5,
-            radio: null
+            radio: null,
         };
     }
 
